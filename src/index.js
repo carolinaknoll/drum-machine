e97deccf9dd4a49c9900d337af2baa1eae2c88ab
Carolina Knoll <carolinasknoll@gmail.com>
2019-08-22 10:33:42 -0300

    Remove unneeded react-router-dom dependency

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter, Route, Switch} from 'react-router-dom';
-
+import DrumMachineContainer from './containers/drum-machine-container';
 import './index.css';
 
-import DrumMachineContainer from './containers/drum-machine-container';
-
 ReactDOM.render(
-  <HashRouter>
-    <div>
-      <Route exact path='/' component={DrumMachineContainer} />
-      <Switch>
-        <Route path='/drum-machine' components={DrumMachineContainer} />
-      </Switch>
-    </div>
-  </HashRouter>,
-  document.getElementById('root'),
+  <DrumMachineContainer/>,
+  document.getElementById('root')
 );
