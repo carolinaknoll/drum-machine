e2144723e5269ec469755663ec9641670761db5b
Carolina Knoll <carolinasknoll@gmail.com>
2019-08-26 11:02:13 -0300

    Add gh-pages support, add webpack config files

--- webpack.common.js
+++ webpack.common.js
@@ -1,16 +1,16 @@
 const path = require("path");
-const webpack = require("webpack");
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.js",
-  mode: "development",
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /node_modules/,
         loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
+        options: {
+          presets: ["@babel/env"]
+        }
       },
       {
         test: /\.css$/,
@@ -32,17 +32,17 @@ module.exports = {
       }
     ]
   },
-  resolve: { extensions: ["*", ".js", ".jsx"] },
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  },
   output: {
     path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/",
+    publicPath: "/",
     filename: "bundle.js"
   },
-  devServer: {
-    contentBase: path.join(__dirname, "public/"),
-    port: 3000,
-    publicPath: "http://localhost:3000/dist/",
-    hotOnly: true
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+  plugins: [
+    new CopyPlugin([{
+      from: 'public'
+    }]),
+  ]
 };
